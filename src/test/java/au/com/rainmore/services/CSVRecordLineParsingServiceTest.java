65787427bf7b68a8bdb648e81c7ba16d60e75988
Felix Rong <frong@infomedia.com.au>
2024-05-04 00:48:22 +1000

    Introduced more tests for CSVRecordLineParsingService

--- src/test/java/au/com/rainmore/services/CSVRecordLineParsingServiceTest.java
+++ src/test/java/au/com/rainmore/services/CSVRecordLineParsingServiceTest.java
@@ -95,4 +95,96 @@ class CSVRecordLineParsingServiceTest {
         assertEquals(Duration.of(4122, ChronoUnit.SECONDS), csvRecord.getBuildDuration());
     }
 
+    @Test
+    void test_fromCSVLine_withInvalidCSVLine() throws Exception {
+        String emptyCSVLine = "";
+
+        ParseException emptyException = assertThrows(ParseException.class, () -> service.fromCSVLine(emptyCSVLine));
+        assertEquals(0, emptyException.getErrorOffset());
+        assertEquals("CSV Line cannot be empty", emptyException.getMessage());
+
+        String invalidCSVLine1 = "customer,2346,eu_west,YellowTeam3,ProjectEgg,4122s,invalid";
+
+        ParseException invalidException1 = assertThrows(ParseException.class, () -> service.fromCSVLine(invalidCSVLine1));
+        assertEquals(0, invalidException1.getErrorOffset());
+        assertEquals("Invalid CSV format", invalidException1.getMessage());
+
+        String invalidCSVLine2 = "customer,2346,eu_west,YellowTeam3,ProjectEgg,";
+
+        ParseException invalidException2 = assertThrows(ParseException.class, () -> service.fromCSVLine(invalidCSVLine2));
+        assertEquals(0, invalidException2.getErrorOffset());
+        assertEquals("Invalid CSV format", invalidException2.getMessage());
+    }
+
+    @Test
+    void test_fromCSVLine_withEmptyCustomerId_or_InvalidCustomerId() throws Exception {
+        String emptyCSVLine = ",2346,eu_west,YellowTeam3,ProjectEgg,4122s";
+
+        ParseException emptyException = assertThrows(ParseException.class, () -> service.fromCSVLine(emptyCSVLine));
+        assertEquals(0, emptyException.getErrorOffset());
+        assertEquals("Customer Id is null or empty", emptyException.getMessage());
+
+        String invalidCSVLine = "customer,2346,eu_west,YellowTeam3,ProjectEgg,4122s";
+
+        ParseException invalidException = assertThrows(ParseException.class, () -> service.fromCSVLine(invalidCSVLine));
+        assertEquals(0, invalidException.getErrorOffset());
+        assertEquals("Customer Id \"customer\" is not a number", invalidException.getMessage());
+    }
+
+    @Test
+    void test_fromCSVLine_withEmptyContractId_or_InvalidContractId() throws Exception {
+        String emptyCSVLine = "3244132,,eu_west,YellowTeam3,ProjectEgg,4122s";
+
+        ParseException emptyException = assertThrows(ParseException.class, () -> service.fromCSVLine(emptyCSVLine));
+        assertEquals(1, emptyException.getErrorOffset());
+        assertEquals("Contract Id is null or empty", emptyException.getMessage());
+
+        String invalidCSVLine = "3244132,contract,eu_west,YellowTeam3,ProjectEgg,4122s";
+
+        ParseException invalidException = assertThrows(ParseException.class, () -> service.fromCSVLine(invalidCSVLine));
+        assertEquals(1, invalidException.getErrorOffset());
+        assertEquals("Contract Id \"contract\" is not a number", invalidException.getMessage());
+    }
+
+    @Test
+    void test_fromCSVLine_withEmptyGeoCode_or_InvalidGeoCode() throws Exception {
+        String emptyCSVLine = "3244132,2346,,YellowTeam3,ProjectEgg,4122s";
+
+        ParseException emptyException = assertThrows(ParseException.class, () -> service.fromCSVLine(emptyCSVLine));
+        assertEquals(2, emptyException.getErrorOffset());
+        assertEquals("Geo Zone is null or empty", emptyException.getMessage());
+
+        String invalidCSVLine = "3244132,2346,eu_west_1,YellowTeam3,ProjectEgg,4122s";
+
+        ParseException invalidException = assertThrows(ParseException.class, () -> service.fromCSVLine(invalidCSVLine));
+        assertEquals(2, invalidException.getErrorOffset());
+        assertEquals("Geo Zone \"eu_west_1\" is invalid", invalidException.getMessage());
+    }
+
+    @Test
+    void test_fromCSVLine_withEmptyTeamCode() throws Exception {
+        String emptyCSVLine = "1233456,2345,us_west,,ProjectDate,2221s";
+
+        ParseException emptyException = assertThrows(ParseException.class, () -> service.fromCSVLine(emptyCSVLine));
+        assertEquals(3, emptyException.getErrorOffset());
+        assertEquals("Team Code is null or empty", emptyException.getMessage());
+    }
+
+    @Test
+    void test_fromCSVLine_withEmptyProjectCode() throws Exception {
+        String emptyCSVLine = "3244132,2346,eu_west,YellowTeam3,,4122s";
+
+        ParseException emptyException = assertThrows(ParseException.class, () -> service.fromCSVLine(emptyCSVLine));
+        assertEquals(4, emptyException.getErrorOffset());
+        assertEquals("Project Code is null or empty", emptyException.getMessage());
+    }
+
+    @Test
+    void test_fromCSVLine_withInvalidDuration() throws Exception {
+        String invalidCSVLine = "3244132,2346,eu_west,YellowTeam3,ProjectEgg,2323";
+
+        ParseException invalidException = assertThrows(ParseException.class, () -> service.fromCSVLine(invalidCSVLine));
+        assertEquals(5, invalidException.getErrorOffset());
+    }
+
 }
