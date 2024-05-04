ffb4a882a3e45f1f458de213498dc34403a5f305
Felix Rong <frong@infomedia.com.au>
2024-05-04 22:45:36 +1000

    Used Gradle application plugin to allow the input from command line

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    application
+    idea
 }
 
 group = "au.com.rainmore"
@@ -9,6 +10,18 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+application {
+    mainClass.set(listOf(project.group.toString(), "Main").joinToString("."))
+}
+
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+}
+
 repositories {
     mavenCentral()
 }
@@ -16,6 +29,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.run.configure {
+    standardInput = System.`in`
 }
 
 tasks.test {
