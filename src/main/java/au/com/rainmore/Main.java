889a345f9e58228845e84bb58455272057d5ff16
Felix Rong <frong@infomedia.com.au>
2024-05-04 23:23:38 +1000

    Updated Main class to use the services

--- src/main/java/au/com/rainmore/Main.java
+++ src/main/java/au/com/rainmore/Main.java
@@ -1,7 +1,35 @@
 package au.com.rainmore;
 
+import au.com.rainmore.domains.CSVRecord;
+import au.com.rainmore.services.CSVRecordsService;
+import au.com.rainmore.services.csv.CSVRecordLineParsingService;
+import au.com.rainmore.services.csv.CSVRecordParsingService;
+import au.com.rainmore.services.reports.ReportService;
+
+import java.text.ParseException;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length != 1) {
+            System.out.println("Please provide CSV string");
+            System.exit(1);
+        }
+
+        CSVRecordParsingService csvRecordParsingService = new CSVRecordParsingService(new CSVRecordLineParsingService());
+        ReportService reportService = new ReportService(new CSVRecordsService());
+
+        try {
+            List<CSVRecord> csvRecords = csvRecordParsingService.parseCSV(args[0]);
+            String report = reportService.buildReport(csvRecords);
+
+            System.out.println(report);
+            System.exit(0);
+        } catch (ParseException ex) {
+            System.out.println(String.format("%s at offset %d", ex.getMessage(), ex.getErrorOffset()));
+            System.exit(1);
+        }
     }
+
 }
